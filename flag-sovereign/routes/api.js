8247f49f0957fec0842527e1e45a9adcfa1a45d3
yoorhim <choi.yoorhim@gmail.com>
2022-07-10 15:43:23 -0400

    Added validator for createFlagset; pseudo code for transforming data from Manager to easily query for SDK

--- flag-sovereign/routes/api.js
+++ flag-sovereign/routes/api.js
@@ -1,6 +1,6 @@
 const express = require ('express');
 const router = express.Router();
-const { validateFlagset } = require('../../validators/validators');
+const { validateFlagset } = require('../validators/validators');
 const { createFlagset } = require("../controllers/flagsetController");
 
 // route to receive webhook from flag manager 
