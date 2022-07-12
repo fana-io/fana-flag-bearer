666e4f1c99e5ba7c33678ecd54cf8cb58ffdcf2e
yoorhim <choi.yoorhim@gmail.com>
2022-07-11 22:17:13 -0400

    Added node-cache (internal caching)

--- flag-sovereign/controllers/cache.js
+++ flag-sovereign/controllers/cache.js
@@ -2,15 +2,26 @@
 // - initialize cache empty
 // - add to cache
 // - look up user flags from cache
-
-const cache = {}
+const NodeCache = require("node-cache");
+const cache = new NodeCache( { stdTTL: 3600, checkperiod: 3620 } ); // what is the difference between stdTTL and checkperiod?
 
 // populate cache for userId
 function populateCacheForUser(sdkKey, userId, flagEvaluations) {
   const userEvals = {};
   userEvals[userId] = { ...flagEvaluations };
-  cache[sdkKey] = userEvals;
+  cache.set(sdkKey, userEvals); // cache[sdkKey][userId] = { flagEval}
 }
 
-module.exports = { populateCacheForUser }
-  
+// cache middleWare
+const checkCache = (req, res, next) => {
+  try {
+    if (cache.has(sdkKey[userId])) {
+      return res.status(200).send(cache.get(sdkKey[userId]))
+    }
+    return next();
+  } catch (err) {
+    throw new Error(err);
+  }
+}
+
+module.exports = { populateCacheForUser, checkCache }
