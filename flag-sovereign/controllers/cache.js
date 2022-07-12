2c356b6bb63b09b55859c9a480c1ce97215925b5
yoorhim <choi.yoorhim@gmail.com>
2022-07-12 10:46:10 -0400

    Added in-memory caching for Client SDK request using node-cache

--- flag-sovereign/controllers/cache.js
+++ flag-sovereign/controllers/cache.js
@@ -3,7 +3,7 @@
 // - add to cache
 // - look up user flags from cache
 const NodeCache = require("node-cache");
-const cache = new NodeCache( { stdTTL: 3600, checkperiod: 3620 } ); // what is the difference between stdTTL and checkperiod?
+const cache = new NodeCache( { stdTTL: 60, checkperiod: 65 } ); // what is the difference between stdTTL and checkperiod?
 
 // populate cache for userId
 function populateCacheForUser(sdkKey, userId, flagEvaluations) {
@@ -14,12 +14,16 @@ function populateCacheForUser(sdkKey, userId, flagEvaluations) {
 
 // cache middleWare
 const checkCache = (req, res, next) => {
+  let sdkKey = req.body.sdkKey
+  let userId = sdkKey.userId
   try {
     if (cache.has(sdkKey[userId])) {
+      console.log("from cache", cache)
       return res.status(200).send(cache.get(sdkKey[userId]))
     }
     return next();
   } catch (err) {
+    console.log("error from cache")
     throw new Error(err);
   }
 }
--- flag-sovereign/routes/api.js
+++ flag-sovereign/routes/api.js
@@ -2,13 +2,14 @@ const express = require ('express');
 const router = express.Router();
 const { validateFlagset, validateClientInit } = require('../validators/validators');
 const { createFlagset, initializeClientSDK, pushFlagUpdates, subscribeToUpdates } = require("../controllers/flagsetController");
+const { checkCache } = require('../controllers/cache');
 
 // route to receive webhook from flag manager
 // also sends push event of disabled flags 
 router.post('/flagset', validateFlagset, pushFlagUpdates, createFlagset);
 
 // receives client SDK initialization requests
-router.post(`/connect/clientInit`, validateClientInit, initializeClientSDK)
+router.post(`/connect/clientInit`, validateClientInit, checkCache, initializeClientSDK)
 
 // endpoint for client SDKs to establish SSE connections
 router.get('/subscribe/client', subscribeToUpdates)
