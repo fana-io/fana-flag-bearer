041ddaa8cda786e36c98ac8329fe2f62b963e22c
audryhsu <audry.hsu@gmail.com>
2022-07-12 21:27:55 -0700

    added: create attribute form (submit disabled)

--- manager-dash/src/components/Attributes/AttributesList.js
+++ manager-dash/src/components/Attributes/AttributesList.js
@@ -1,12 +1,23 @@
 import { attributes } from '../../lib/data';
 import { AttributeListing } from './AttributeListing';
+import { CreateAttributeForm } from "./CreateAttributeForm"
 
 export const AttributesList = () => {
   return (
-    <div className="list">
-      {attributes.map(attribute => {
-        return (<AttributeListing key={attribute.name} attributeDetails={attribute} />)
-      })}
-    </div>
-  )
-}
+    <>
+      <div>
+        <CreateAttributeForm></CreateAttributeForm>
+      </div>
+      <div className="list">
+        {attributes.map((attribute) => {
+          return (
+            <AttributeListing
+              key={attribute.name}
+              attributeDetails={attribute}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
+};
