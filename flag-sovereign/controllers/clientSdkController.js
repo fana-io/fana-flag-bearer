9dbdc4369dd4b885ff7b5094a06ae6996b3dd774
yoorhim <choi.yoorhim@gmail.com>
2022-07-21 17:30:42 -0400

    Updated evaluation with new data structure, sans value as array

--- flag-sovereign/controllers/clientSdkController.js
+++ flag-sovereign/controllers/clientSdkController.js
@@ -5,26 +5,14 @@ const { evaluateFlags } = require('../utils/parseFlagData');
 const initializeClientSDK = (req, res) => {
   const errors = validationResult(req);
   if (errors.isEmpty()) {
-<<<<<<< HEAD
     const sdkKey = req.header('Authorization');
     const userContext = req.body;
-    // validate sdkKey
+    // validate sdkKey; validateSdk not written yet
     // if (!validSdkKey(sdkKey)) {
     //   return res.status(400).send({ error: 'Invalid SDK key.' });
     // }
     const userFlagEvals = evaluateFlags(userContext);
-=======
-    const allFlags = flagData.getFlagData();
-    const sdkKey = req.header('Authorization');
-
-    const sdkInstance = getSdkInstance(sdkKey, allFlags);
-
-    if (!sdkInstance) {
-      return res.status(400).send({ error: 'Invalid SDK key.' });
-    }
-    const userFlagEvals = evaluateFlags(sdkInstance, req.body.userContext);
     // populateCacheForUser(req.body.sdkKey, userId, userFlagEvals);
->>>>>>> 73a603c574accdb0a2ee809d3efd685e1f2fe7db
     return res.json(userFlagEvals);
   } else {
     return res
