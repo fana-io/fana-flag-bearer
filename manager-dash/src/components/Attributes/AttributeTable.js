b71985c554f72e0403164437728081c28b196093
juanjuy <alex.juanjuy@gmail.com>
2022-07-21 01:58:28 -0700

    minor cleanup

--- manager-dash/src/components/Attributes/AttributeTable.js
+++ manager-dash/src/components/Attributes/AttributeTable.js
@@ -2,7 +2,6 @@ import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import Table from "@mui/material/Table";
 import TableContainer from "@mui/material/TableContainer";
 import { AttributeRow } from "./AttributeRow"
 
