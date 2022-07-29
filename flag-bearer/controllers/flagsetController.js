969d29e14cacf58d3a438e7c5d9aa3968412b784
audryhsu <audry.hsu@gmail.com>
2022-07-29 12:49:26 -0700

    clean up before merge into main

--- flag-bearer/controllers/flagsetController.js
+++ flag-bearer/controllers/flagsetController.js
@@ -1,20 +1,9 @@
 const { validationResult } = require('express-validator');
-// const { flagData } = require('../lib/FlagData');
-const { getRuleset } = require('../utils/apiClient')
-
-// const getFlagset = async () => {
-//   let updatedFlagData = await getRuleset();
-//   // to do: get flag data from redis
-//   flagData.setFlagData(updatedFlagData);
-//   return
-// };
 
 const createFlagset = (req, res, next) => {
   const errors = validationResult(req);
   // checks: flatset is array, sdkkey and flags array are provided
   if (errors.isEmpty()) {
-    // redis will be written by manager
-    // flagData.setFlagData(req.body);
     res.status(201).send('201: Flagset created');
   } else {
     return res.status(404).send('Input field error.');
@@ -23,5 +12,4 @@ const createFlagset = (req, res, next) => {
 
 module.exports = {
   createFlagset,
-  // getFlagset,
 };
