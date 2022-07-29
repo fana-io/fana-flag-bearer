ba2dd3e25a48be705f2ab87bccf1adaf5321d576
audryhsu <audry.hsu@gmail.com>
2022-07-29 13:13:04 -0700

    removed manager-dash

--- manager-dash/src/lib/messages.js
+++
@@ -1,9 +0,0 @@
-export const duplicateErrorMessage = "That key is already in use. Please use a different one.";
-
-export const generalErrorMessage = "Something went wrong. Please try again later.";
-
-export const initializationErrorMessage = "Error initializing. Check the connection to the backend manager and try again.";
-
-export const deletedEntityMessageCreator = (entity, key) => {
-  return `The ${entity} ${key} has been successfully deleted.`;
-}
